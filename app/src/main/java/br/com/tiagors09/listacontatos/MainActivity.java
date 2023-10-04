9e093d31a36185267297a76d9b4d2f864bec62e9
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-04 15:59:26 -0300

    adicionado escolha de itens, exclusão, edição e adição de novos contatos

--- app/src/main/java/br/com/tiagors09/listacontatos/MainActivity.java
+++ app/src/main/java/br/com/tiagors09/listacontatos/MainActivity.java
@@ -6,55 +6,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
+import java.util.List;
 
+import br.com.tiagors09.dao.ContatoDAO;
 import br.com.tiagors09.model.Contato;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonAdicionar;
     EditText editTextNome, editTextTelefone, editTextEndereco;
     ListView listViewContatos;
 
+    ContatoDAO contatoDAO = ContatoDAO.getInstance();
     ArrayAdapter<Contato> adapterContatos;
-    ArrayList<Contato> listaContatos;
 
     int posSelection;
+    Contato c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        posSelection = -1;
+
+        buttonAdicionar = findViewById(R.id.adicionarBtn);
+
         editTextNome = findViewById(R.id.editTextNome);
         editTextTelefone = findViewById(R.id.editTextTelefone);
         editTextEndereco = findViewById(R.id.editTextEndereco);
-        listViewContatos = (ListView) findViewById(R.id.listViewContatos);
+        listViewContatos = findViewById(R.id.listViewContatos);
 
-        listaContatos = new ArrayList<Contato>();
+        contatoDAO.create(new Contato("João", "123456789", "Rua A"));
+        contatoDAO.create(new Contato("Maria", "987654321", "Rua B"));
+        contatoDAO.create(new Contato("Carlos", "555555555", "Rua C"));
 
         adapterContatos = new ArrayAdapter<Contato>(
                 this,
                 android.R.layout.simple_list_item_activated_1,
-                listaContatos
+                android.R.id.text1,
+                contatoDAO.getAll()
         );
 
         listViewContatos.setAdapter(adapterContatos);
+        listViewContatos.setOnItemClickListener((adapterView, view, position, id) -> {
+            buttonAdicionar.setEnabled(false);
 
-        listViewContatos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                listViewContatos.setItemChecked(position, false);
+            posSelection = position;
 
-                if (position == posSelection) {
-                    posSelection = -1;
-                } else  {
-                    posSelection = position;
-                }
-            }
+            c = contatoDAO.read(position);
+
+            editTextNome.setText(c.getNome());
+            editTextEndereco.setText(c.getEndereco());
+            editTextTelefone.setText(c.getTelefone());
         });
+    }
+
+    private void limparCampos() {
+        editTextNome.setText("");
+        editTextEndereco.setText("");
+        editTextTelefone.setText("");
     }
 
     public void adicionarContato(View v) {
@@ -64,14 +81,42 @@ public class MainActivity extends AppCompatActivity {
                 editTextEndereco.getText().toString()
         );
 
-        listaContatos.add(c);
+        limparCampos();
+
+        contatoDAO.create(c);
     }
 
     public void editarContato(View v) {
-
+        if (posSelection != -1) {
+            contatoDAO.update(posSelection, c);
+            posSelection = -1;
+            limparCampos();
+            buttonAdicionar.setEnabled(true);
+        } else {
+            Toast
+                    .makeText(
+                            getApplicationContext(),
+                            "Selecione um contato válido",
+                            Toast.LENGTH_SHORT
+                    )
+                    .show();
+        }
     }
 
     public void removerContato(View v) {
-
+        if(posSelection != -1) {
+            contatoDAO.delete(posSelection);
+            posSelection = -1;
+            limparCampos();
+            buttonAdicionar.setEnabled(true);
+        } else {
+            Toast
+                    .makeText(
+                            getApplicationContext(),
+                            "Selecione um contato válido",
+                            Toast.LENGTH_SHORT
+                    )
+                    .show();
+        }
     }
 }
